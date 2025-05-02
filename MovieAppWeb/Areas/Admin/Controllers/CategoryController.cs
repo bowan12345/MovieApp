79d196e23d0399f3c5005feed03b24fed90eedd1
bowan12345 <bowan.ding@gmail.com>
2025-05-02 16:45:50 +1200

    update category features

--- MovieAppWeb/Areas/Admin/Controllers/CategoryController.cs
+++ MovieAppWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using MovieApp.DataAccess.Repository.IRepository;
 using MovieApp.Models;
 using MovieApp.Utility;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace MovieAppWeb.Areas.Admin.Controllers
 {
@@ -18,99 +19,82 @@ namespace MovieAppWeb.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            List<Category> categories = _unitOfWork.categoryRepository.GetAll().ToList();
-            return View(categories);
+            List<Category> categorys = _unitOfWork.categoryRepository.GetAll().ToList();
+            return View(categorys);
         }
 
-        //return a create page
-        public IActionResult Create()
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            return View();
+            List<Category> categorys = _unitOfWork.categoryRepository.GetAll().ToList();
+            return Json(new { data = categorys });
+
+        }
+
+
+        //return a create/update page
+        public IActionResult Upsert(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                //create
+                return View(new Category());
+            }
+            else
+            {
+                //update
+                Category category = _unitOfWork.categoryRepository.Get(u => u.Id == id);
+                return View(category);
+            }
         }
 
         [HttpPost]
         public IActionResult Create(Category _category)
         {
-            if (_category.Name.Equals(_category.DisplayOrder.ToString()))
-            {
-                ModelState.AddModelError("Name", "The Display Order cannot exactly match the name ");
-            }
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.categoryRepository.Add(_category);
+                //create
+                if (_category.Id == 0)
+                {
+                    //check the duplicated name
+                    Category category = _unitOfWork.categoryRepository.Get(u => u.Name == _category.Name);
+                    if (category != null)
+                    {
+                        TempData["error"] = "Category Name Already Exist!!!";
+                        return View("Upsert", _category);
+                    }
+                    _unitOfWork.categoryRepository.Add(_category);
+                    TempData["success"] = "Category created successfully";
+                }
+                else
+                {
+                    //update
+                    _unitOfWork.categoryRepository.Update(_category);
+                    TempData["success"] = "Category updated successfully";
+                }
                 _unitOfWork.Save();
-                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+                return View(_category);
+            }
         }
 
-        //edit button
-        public IActionResult Edit(int id)
+        [HttpDelete]
+        public IActionResult Delete(int? id)
         {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
+
             Category category = _unitOfWork.categoryRepository.Get(u => u.Id == id);
             if (category == null)
             {
-                return NotFound();
-            }
-            return View(category);
-        }
-
-
-        [HttpPost]
-        public IActionResult Edit(Category _category)
-        {
-            if (_category.Name.Equals(_category.DisplayOrder.ToString()))
-            {
-                ModelState.AddModelError("Name", "The Display Order cannot exactly match the name ");
-            }
-
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.categoryRepository.Update(_category);
-                _unitOfWork.Save();
-                TempData["success"] = "Category updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
-
-        //Delete button
-        public IActionResult Delete(int id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Category category = _unitOfWork.categoryRepository.Get(u => u.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-            return View(category);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteGet(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Category category = _unitOfWork.categoryRepository.Get(u => u.Id == id);
-            if (category == null)
-            {
-                return NotFound();
+                return Json(new { success = false, message = "Category not found" });
             }
             _unitOfWork.categoryRepository.Remove(category);
             _unitOfWork.Save();
-            TempData["success"] = "Category Deleted successfully";
-            return RedirectToAction("Index");
+            return Json(new { success = true, message = "Delete successfully" });
         }
     }
 }
---
+++ MovieAppWeb/wwwroot/js/category.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    datatable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/category/getall' },
+        "columns": [
+            { data: 'name', "width": "20%" },
+            { data: 'displayOrder', "width": "15%" },
+            { data: 'description', "width": "45%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/category/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                     <a onClick=deleteItem('/admin/category/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "20%"
+            }
+        ]
+
+    });
+}
+
+function deleteItem(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    console.log(data);
+                    datatable.ajax.reload();
+                    //loadDataTable();
+                    //toastr.success(data.message);
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: data.message,
+                        icon: "success"
+                    });
+                }
+            })
+        }
+    });
+
+}
